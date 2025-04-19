578d8958d2f4e80defb82aa9c81047bfcdeb6262
Trung hua <trunghua75@gmail.com>
2025-04-19 14:43:26 +0000

    Add Infra workflow v3

--- app.tf/variables.tf
+++ app.tf/variables.tf
@@ -65,3 +65,20 @@ variable "create_flow_log_cloudwatch_log_group" {
 variable "eks_config" {
 
 }
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
