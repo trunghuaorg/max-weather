a4b0794ea1d36bd4c31270353d9b0bef6e8305c6
Trung hua <trunghua75@gmail.com>
2025-04-19 15:24:42 +0000

    Add Infra workflow v3

--- app.tf/variables.tf
+++ app.tf/variables.tf
@@ -67,19 +67,13 @@ variable "eks_config" {
 }
 
 variable "aws_region" {
-  description = "AWS Region"
-  type        = string
-  default     = "eu-west-1"  
+
 }
 
 variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-  sensitive   = true
+
 }
 
 variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-  sensitive   = true
+
 }
